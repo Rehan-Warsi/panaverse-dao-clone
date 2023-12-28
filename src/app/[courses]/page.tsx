00431dc421faa57254ef5331d1ad8d240f4eb245
Rehan Warsi <rehan.waarsi@gmail.com>
2023-12-29 03:13:47 +0500

    panaverse-dao-clone

--- src/app/[courses]/page.tsx
+++ src/app/[courses]/page.tsx
@@ -1,13 +1,22 @@
+import { programData } from "@/Components/Widgets/SpecializedTrack";
+
+const Page = (slug: string) => {
+      const selectedData = programData.find((item)=> item.slug === slug)
+  console.log(slug);
   
-
-const Page = ( slug:string) => {
-     console.log(slug)
   return (
-    
-        <h3>profile</h3>
-    
-  )
-}
+    <div className="mt-10 flex gap-x-6 gap-y-8 flex-col-reverse lg:flex-row ">
+      <div className="shadow-xl border border-slate-200 rounded-xl px-8 py-8 basis-8/12 sticky top-28 self-start">
+        <h3 className="text-primary text-lg font-medium mt-4">
+          Specialized Track
+        </h3>
+        <h1 className="text-2xl font-bold">{selectedData?.header}</h1>
+        <p className="text-lg text-slate-600 mt-3">
+          {selectedData?.description}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Page
-
+export default Page;
